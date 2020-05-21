d64391cb0efc9b0d13b805b61fcf52ff48cf7aed
Nancy Tochi O <nancytochi24@gmail.com>
2020-05-21 18:59:26 -0400

    Update navbar scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
+//Navbar transition
+$(window).scroll(function(){
+	$('nav').toggleClass('scrolled', $(this).scrollTop() > 250);
+})
 
+//Carousal
 $(document).ready(function(){
     $(".carousal").carousel({interval: 90,});
 })
